ddd9ee92b076171bcd905916b6d952b10ae1a254
rjwilson0531 <rjwilson0531@gmail.com>
2020-03-11 16:27:03 -0400

    ocmmit message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded",fetchDogs)
+
+function fetchDogs() {
+    fetch(getUrl())
+        .then(response => response.json())
+        .then(dogs => dogs.forEach(dog => addImg(dog)))
+}
+
+function getUrl() {
+    return "https://dog.ceo/api/breeds/image/random/4"
+}
+
+function addImg(dog) {
+    let dogContainer = document.getElementById('dog-image-container');
+    let dogImage = document.createElement('img')
+    dogImage.src = dog;
+    dogContainer.appendChild(dogImage)
+}
