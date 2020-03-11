ef10495392e766fd4c9f147a5f53b9d5fe1aab36
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-11 16:52:32 -0400

    Challenge 2 Completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded",fetchDogs)
+document.addEventListener("DOMContentLoaded",fetchDogsImgs)
 
-function fetchDogs() {
+function fetchDogsImgs() {
     fetch(getUrl())
         .then(response => response.json())
-        .then(dogs => dogs.forEach(dog => addImg(dog)))
+        .then(dogs => dogs.message.forEach(dog => addImg(dog)))
 }
 
 function getUrl() {
@@ -15,4 +15,19 @@ function addImg(dog) {
     let dogImage = document.createElement('img')
     dogImage.src = dog;
     dogContainer.appendChild(dogImage)
+}
+
+function fetchDogsBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => response.json())
+        .then(dogs => Object.keys(dogs.message).forEach(dog => getBreed(dog)))
+
+        // dogs.message.forEach(dog => getBreed(dog))
+}
+
+function getBreed(dog) {
+    let dogBreedList = document.getElementById('dog-breeds');
+    let dogBreed = document.createElement('li')
+    dogBreed.innerText = dog;
+    dogBreedList.appendChild(dogBreed)
 }
